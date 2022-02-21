debc2c5e22e8646e525ed2056ecc59a5bef7499f
samundar <samundarsingh9525@gmail.com>
2022-02-21 10:34:09 +0530

    click event complemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { MyserviceService } from './services/myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'toolti';
+export class AppComponent implements AfterViewInit {
+  title = 'toolti' ;
+  @ViewChild("tabtoogle") tabtoogle:any;
+  constructor(private mystl:MyserviceService) {
+  }
+  ngAfterViewInit(): void {
+    fromEvent(this.tabtoogle.nativeElement,'').subscribe(
+      res=>{
+        console.log("hello");
+        this.mystl.tab_toggle_keyup("myco");
+
+      }
+    )
+
+
+
+  }
+
 }
